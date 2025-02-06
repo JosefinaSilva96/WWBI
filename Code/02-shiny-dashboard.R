39e00e6f90d279874955cbf4033d5953017e4d47
JosefinaSilva96 <josefinasilvafuente@gmail.com>
2025-02-06 14:55:29 -0500

    changes

--- Code/02-shiny-dashboard.R
+++ Code/02-shiny-dashboard.R
@@ -654,7 +654,8 @@ gender_leadership <- gender_leadership %>%
 
 # Define UI ----
 
-ui <- dashboardPage(
+ui <- bootstrapPage(
+  theme = bs_theme(version = 5, bootswatch = 'quartz'),
   dashboardHeader(title = "WWB Indicators"),
   dashboardSidebar(
     sidebarMenu(
@@ -2926,3 +2927,35 @@ ui <- bs4DashPage(
 server <- function(input, output) {}
 
 shinyApp(ui, server)
+
+
+
+ui <- bootstrapPage(
+  theme = bs_theme(version = 5, bootswatch = 'quartz'),
+  titlePanel("Old Faithful Geyser Data"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    hist(x, breaks = bins, col = 'darkgray', border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = 'Histogram of waiting times')
+  })
+}
+
+shinyApp(ui = ui, server = server)
